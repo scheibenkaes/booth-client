f2e0c7036bec15b252b08f5ffa140f33dff12bdf
Benjamin Klüglein <scheibenkaes@googlemail.com>
2014-09-18 20:00:23 +0200

    run a given command when some line comes in from *in*

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://github.com/scheibenkaes/booth-client"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
+                 [org.clojure/tools.cli "0.3.1"]]
   :main ^:skip-aot booth-client.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/booth_client/core.clj
+++ src/booth_client/core.clj
@@ -1,7 +1,42 @@
 (ns booth-client.core
+  (:require [clojure.core.async :refer [chan go-loop <!! >!!]]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.java.io :as io]
+            [clojure.java.shell :as shell]
+            )
   (:gen-class))
 
+(def cli-options
+  [["-c" "--command COMMAND" "Command to run"
+    :id :command
+    :default "echo running"]
+   ])
+
+(def notification-received-chan (chan))
+
+(defn start-listening-for-notifications [notify-chan]
+  (let [reader (-> *in* io/reader)]
+    (go-loop []
+             (try
+               (let [data (.readLine reader)]
+                 (when data
+                   (>!! notify-chan data))
+                 (Thread/yield))
+               (catch Exception e
+                 (println
+                  (.getMessage e))))
+             (recur)
+             )))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{arguments :arguments
+         {cmd :command} :options} (parse-opts args cli-options)]
+    (println "Starting...")
+    (go-loop []
+             (let [data (<!! notification-received-chan)]
+               (apply println "Running command " cmd arguments)
+               (apply shell/sh (concat [cmd] arguments))
+               (recur)))
+    (<!!
+     (start-listening-for-notifications notification-received-chan))))
